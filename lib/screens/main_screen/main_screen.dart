6d9e3d8069d38b0f34c41c040e833bd1f4598f1d
AnthonyAniobi <anthonyaniobi198@gmail.com>
2022-10-03 23:30:50 +0100

    added resources from previous  build

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -65,7 +65,7 @@ class MainScreen extends StatelessWidget {
         duration: Duration(milliseconds: 200),
       ),
       navBarStyle:
-          NavBarStyle.style1, // Choose the nav bar style with this property.
+          NavBarStyle.style6, // Choose the nav bar style with this property.
     );
   }
 }
